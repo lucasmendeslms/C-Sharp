7716c91aaffb3546013c4f6fc77dd415d99a571d
Lucas Mendes <lucas.mendessilva@hotmail.com>
2022-11-22 23:39:13 -0300

    Projeto concluído

---
+++ MaiorIdade/MaiorIdade/Person.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiorIdade
+{
+    public class Person
+    {
+        public Person() { }
+
+        DateTime date = DateTime.Now;
+
+        public Person(String name, DateTime birthDate)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+
+        private String _name;
+
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private DateTime _birthDate;
+
+        public DateTime BirthDate
+        {
+            get { return _birthDate; }
+            set { _birthDate = value; }
+        }
+
+        private int _age;
+
+        public int Age
+        {
+            get
+            {
+                this.CalculateAge();
+                return this._age;
+            }
+        }
+
+        private String _ofAge;
+
+        public String OfAge
+        {
+            get
+            {
+                this.CalculateAge();
+                return this._ofAge;
+            }
+        }
+
+        private String _isBirthDay;
+
+        public String IsBirthDay
+        {
+            get
+            {
+                this.isBirthDay();
+                return this._isBirthDay;
+            }
+        }
+
+        private void CalculateAge()
+        {
+            TimeSpan yearsOld = date.Subtract(this.BirthDate);
+            this._age = int.Parse(yearsOld.Days.ToString()) / 365;
+
+            if (this._age >= 18)
+            {
+                this._ofAge = $"{this.Name} é maior de idade";
+            } 
+            
+            else
+            {
+                this._ofAge = "";
+            }
+
+        }
+
+        private void isBirthDay()
+        {
+            if (_birthDate.Month == date.Month && _birthDate.Day == date.Day)
+            {
+                this._isBirthDay = $"Hoje é seu aniversário, parabéns {this.Name}";
+            }
+
+            else
+            {
+                this._isBirthDay = "";
+            }
+        }
+
+        public void ExhibitData()
+        {
+            Console.WriteLine($"Nome: {this.Name}");
+            Console.WriteLine($"Data de aniversário: {this.BirthDate.ToShortDateString()}");
+            Console.WriteLine($"Idade: {this.Age}");
+            Console.WriteLine(this.IsBirthDay);
+            Console.WriteLine(this.OfAge);
+        }
+
+    }
+}
---
+++ MaiorIdade/MaiorIdade/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MaiorIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantas pessoas você deseja cadastrar? ");
+            int quantityPeople = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Person[] person = new Person[quantityPeople];
+
+            Console.WriteLine("Controle de pessoas\n");
+
+            for (int i = 0; i < quantityPeople; i++) 
+            {
+                person[i] = new Person();
+
+                Console.Clear();
+
+                Console.Write($"Informe o nome da {i + 1}ª pessoa: ");
+                person[i].Name = Console.ReadLine();
+
+                Console.Clear();
+
+                Console.Write($"Informe a data de aniversário da {i + 1}ª pessoa - Formato dd/mm/aaaa: ");
+                person[i].BirthDate = DateTime.Parse(Console.ReadLine());
+
+                Console.Clear();
+            }
+
+
+            foreach(Person people in person)
+            {
+                people.ExhibitData();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
